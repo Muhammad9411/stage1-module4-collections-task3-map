7f5fae9076d5c4b7ad79fa004f00091f74d0346f
MuhammadJon <mtjohn9411@gmail.com>
2023-04-04 16:46:04 +0500

    Stage 1 Map Interface Tasks solution has been developed

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -1,8 +1,21 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class KeyValueSwapper {
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
+
+        Map<String, Integer> swap = new HashMap<>();
+
+        for (Map.Entry<Integer, String> e : sourceMap.entrySet()) {
+            Integer oldKey = e.getKey();
+            String oldVal = e.getValue();
+            if (!swap.containsKey(oldVal)) {
+                swap.put(oldVal, oldKey);
+            }
+        }
+
+        return swap;
     }
 }
--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,29 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+
+        Map<String, Integer> map = new HashMap<>();
+
+        if (sentence.isEmpty()) {
+            return map;
+        }
+
+        String s_arr[] = sentence.replaceAll("[\\,.]", "").split(" ");
+
+        for (int i = 0; i < s_arr.length; i++) {
+            int value = 1;
+            String m_key = s_arr[i].toLowerCase();
+            if (map.containsKey(m_key)) {
+                map.put(m_key, Integer.parseInt(map.get(m_key).toString()) + value);
+            } else {
+                map.put(s_arr[i].toLowerCase(), value);
+            }
+        }
+
+        return map;
     }
 }
